39bc380124592ead972e2aac52af1e0d886fa67a
Антон Плотицын <plotitsynanton@yandex.ru>
2023-09-24 16:12:02 +0300

    решение некоторых задач

--- tasks/task_6.py
+++ tasks/task_6.py
@@ -1,1 +1,25 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+# num: int = int(input('Введите целое число: '))
+# total_gigit = 0
+# while num > total_gigit:
+#     total_gigit = total_gigit + num
+#     num += 1
+#     print(total_gigit - 1)
+
+# user_input = int(input("Введите целое число: "))
+# num = str(user_input).split(' ')
+# for i in num:
+#     print(num)
+
+# for i in range(user_input + 1):
+#     print(i, end=' ')
+
+
+number: int = abs(int(input('Введите целое число: ')))
+if number > 0:
+    count = 0
+    while number > 0:
+        count += 1
+        number = number // 10
+    print("Количество цифр в числе:", count)
+else:
+    print("Количество цифр в числе:", 1)
