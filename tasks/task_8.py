07f51b3173930cabae8d8b7d8e11d90217491a7a
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-10-01 11:19:23 +0100

    Проверка ДЗ | 7. Циклы
    - Правильно: №4, №5, №6,
    - Неправильно: №8,
    - Улучшены: №5, №6, №9
    - На доработку: №8,

--- tasks/task_8.py
+++ tasks/task_8.py
@@ -1,15 +1,18 @@
 start_num: int = int(input('Введите начало диапазона: '))
 end_num: int = int(input('Введите конец диапазона: '))
 
-if start_num > end_num:
+if start_num > end_num:  # Отлично!
     start_num, end_num = end_num, start_num
 
+# Эти условия должны быть реализованы внутри цикла (в цикле будет только if elif)
 if start_num == end_num:
-    if start_num % 2 == 0:
+    if start_num % 2 == 0:  # Это ты уже реализовал в цикле
         print(start_num)
     else:
         print(0)
-else:
-    for num in range(start_num, end_num + 1):
-        if num % 2 == 0:
-            print(num)
+
+for num in range(start_num, end_num + 1):
+    if num % 2 == 0:  # Это правильно
+        print(num)
+    elif ...:
+        ...
