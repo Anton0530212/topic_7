90eb619228072d59994cbaccb326e03515f37c38
Антон Плотицын <plotitsynanton@yandex.ru>
2023-10-03 13:20:08 +0300

    - Сделана: №7

--- tasks/task_7.py
+++ tasks/task_7.py
@@ -1,1 +1,10 @@
-# Шох не понимаю математику в этой задаче
+num: int = int(input("Введите число: "))
+
+primes: list = []
+
+for i in range(2, num + 1):
+    while num % i == 0:
+        primes.append(i)
+        num /= i
+
+print(" ".join(map(str, primes)))
--- tasks/task_8.py
+++ tasks/task_8.py
@@ -1,18 +1,11 @@
 start_num: int = int(input('Введите начало диапазона: '))
 end_num: int = int(input('Введите конец диапазона: '))
 
-if start_num > end_num:  # Отлично!
+if start_num > end_num:
     start_num, end_num = end_num, start_num
 
-# Эти условия должны быть реализованы внутри цикла (в цикле будет только if elif)
-if start_num == end_num:
-    if start_num % 2 == 0:  # Это ты уже реализовал в цикле
-        print(start_num)
-    else:
-        print(0)
-
 for num in range(start_num, end_num + 1):
-    if num % 2 == 0:  # Это правильно
+    if num % 2 == 0:
         print(num)
-    elif ...:
-        ...
+    elif start_num == end_num:
+        print(0)
